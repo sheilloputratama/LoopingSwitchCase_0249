190ed146bfdb82d5c938eccc1df7ffb56202decc
sheilloputratama <sheilloggendut@gmail.com>
2025-03-17 19:54:24 +0700

    menambahLooping

---
+++ ContohLooping.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int counter;
+    int i;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+    
+    //Loop with for
+    //Loop for ascending
+    for (counter = 0; counter < 5; counter++)
+    {
+        cout << "Nilai Counter = " << counter << " TI UMY" << endl;
+    }
+    cout << "Nilai Counter terakhir = " << counter << endl;
+    cout << endl;
+
+    //Loop for descending
+    for (counter = 5; counter < 0; counter--)
+    {
+        cout << "Nilai Counter = " << counter << " TI UMY" << endl;
+    }
+    cout << "Nilai Counter terakhir = " << counter << endl;
+    cout << endl;
+
+
+    //Loop with while
+    srand(time(0));
+    i = rand() % 10;
+
+    cout << "Nilai awal i " << i << endl;
+    while (i < 7)
+    {
+        cout << "Nilai awal i = " << i << "TI UMY" << endl;
+        i = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai akhir i = " << i << endl;
+
+    cout << endl;
+
+    //Loop do while
+    cout << "Perulangan Do while"<< endl;
+    do
+    {
+        cout << "Nilai i = " << i << "TI UMY" << endl;
+        i = rand() % 10;
+        perulanganWhile;
+    } while (i < 7);
+    cout << "Jumlah perulangan = " << perulanganDo << endl;
+    cout << "Nilai akhir i = " << i << endl;
+     
+
+}
