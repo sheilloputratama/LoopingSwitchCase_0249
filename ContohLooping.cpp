eb1c6c40ecabcbc0344f19bcaf6e97775fc7677c
sheilloputratama <sheilloggendut@gmail.com>
2025-03-17 20:10:07 +0700

    menambah Library

--- ContohLooping.cpp
+++ ContohLooping.cpp
@@ -2,30 +2,6 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
-int main() {
-    int counter;
-    int i;
-    int perulanganWhile = 0;
-    int perulanganDo = 0;
-    
-    //Loop with for
-    //Loop for ascending
-    for (counter = 0; counter < 5; counter++)
-    {
-        cout << "Nilai Counter = " << counter << " TI UMY" << endl;
-    }
-    cout << "Nilai Counter terakhir = " << counter << endl;
-    cout << endl;
- //Loop for descending
- for (counter = 5; counter < 0; counter--)
- {
-     cout << "Nilai Counter = " << counter << " TI UMY" << endl;
- }
- cout << "Nilai Counter terakhir = " << counter << endl;
- cout << endl;
-
    
      
    
-}
