a7eb8a83039d2715dd417c3c2870b09d99f619d8
sheilloputratama <sheilloggendut@gmail.com>
2025-03-17 20:17:35 +0700

    example for

--- ContohLooping.cpp
+++ ContohLooping.cpp
@@ -10,4 +10,13 @@ int main() {
     int perulanganWhile = 0;
     int perulanganDo = 0;
     
- 
+    //Loop with for
+    //Loop for ascending
+    for (counter = 0; counter < 5; counter++)
+    {
+        cout << "Nilai Counter = " << counter << " TI UMY" << endl;
+    }
+    cout << "Nilai Counter terakhir = " << counter << endl;
+    cout << endl;
+
+
